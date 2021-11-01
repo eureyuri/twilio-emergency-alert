fd96ebbf5e7d668bdc02a3922b998cdbc5765c76
eureyuri <eureynoguchi@gmail.com>
2021-11-01 15:14:36 -0400

    feat: add async task

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn app:app
+beat: celery -A tasks beat --loglevel=info
+worker: celery -A tasks worker
+web: gunicorn app:flask_app
--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, request, redirect, session, render_template
-from twilio.twiml.messaging_response import MessagingResponse
 import json
 import os
+from datetime import datetime, timedelta
+
+from flask import Flask, request, redirect, session, render_template
+from twilio.twiml.messaging_response import MessagingResponse
+from celery import Celery
+import pandas as pd
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# To start
+# heroku ps:scale worker=1
+# heroku ps:scale beat=1
+
+# To stop
+# heroku ps:scale worker=0
+# heroku ps:scale beat=0
+
+
 app = Flask(__name__)
 app.secret_key = "super secret key"
+app.config['CELERY_BROKER_URL'] = os.environ.get('CLOUDAMQP_URL')
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
 
 # Initialize Firestore DB
 firebase_key = os.environ.get('FIREBASE_KEY', 'sms-emergency-alert-firebase-key.json')
@@ -24,9 +42,24 @@ with open('questions.json') as json_file:
     questions = json.load(json_file)
 
 
+@celery.task
+def emergency_check():
+    resp = MessagingResponse()
+    resp.message('Checking!')
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "Emergency Alert is running!"
+
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     resp = MessagingResponse()
+
+    time_limit = datetime.utcnow() + timedelta(hours=0, minutes=1)
+    emergency_check.apply_async(eta=time_limit)
+
     ### TODO: cover edge cases of return after completion (lead to key error -1 for now)
     req_body = request.values.get('Body')
     if req_body == "restart":  # for testing purpose
@@ -40,11 +73,26 @@ def sms_reply():
         log_data_firestore('0', resp_txt, req_body)
     else:
         question_id = session['question_id']
-        # handle the request and send the next question
-        next_id = questions[str(question_id)]["next_question"]
+
+        # TODO: If question is 3 then set reminder + 5 min
+        if int(question_id) is 3:
+            time_given = pd.to_datetime(req_body, format='%Hh%Mm')
+            print(time_given)
+            h = time_given.hour
+            m = time_given.minute
+            time_limit = datetime.utcnow() + timedelta(hours=h, minutes=m)
+            emergency_check.apply_async(eta=time_limit)
+
+        # TODO: If question is 4 then cancel task
+        # TODO: If reminder goes off then send emergency alert
+
+
+        # Send a response and log data
         resp_txt = questions[question_id]["text"]
         msg = resp.message(resp_txt)
         log_data_firestore(question_id, resp_txt, req_body)
+
+        next_id = questions[str(question_id)]["next_question"]
         session['question_id'] = next_id
     return str(resp)
 
@@ -95,3 +143,4 @@ def user_details(user_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.start()
