76ecf61d633602640d599f510a8cef22b91b251f
eureyuri <eureynoguchi@gmail.com>
2021-11-02 13:04:14 -0400

    for demo

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ def check_in(name, to, emergency_number, text):
     )
 
     # Text emergency after 5 min grace period
-    time_limit = datetime.utcnow() + timedelta(minutes=5)
+    # FIXME
+    # time_limit = datetime.utcnow() + timedelta(minutes=5)
+    time_limit = datetime.utcnow() + timedelta(minutes=1)
     EMERGENCY_JOB = scheduler.add_job(func=emergency_notice,
                                       args=[name, to, emergency_number],
                                       trigger="date",
@@ -113,7 +115,9 @@ def sms_reply():
             h = time_given.hour
             m = time_given.minute
 
-            time_limit = datetime.utcnow() + timedelta(hours=h, minutes=m)
+            # FIXME
+            # time_limit = datetime.utcnow() + timedelta(hours=h, minutes=m)
+            time_limit = datetime.utcnow() + timedelta(seconds=5)
             JOB_ID = scheduler.add_job(func=check_in,
                                        args=[session['name'], session['from_number'],
                                              session['emergency_number'], resp_txt['check']],
